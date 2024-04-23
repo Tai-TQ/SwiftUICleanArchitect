acbee2c910bde7aac00f6d36fdead8ff80a87309
Truong Quoc Tai <truong.quoc.tai@sun-asterisk.com>
2024-04-23 10:51:28 +0700

    Create PopupManager

---
+++ Sources/Presentation/View/PopupManager.swift
@@ -0,0 +1,30 @@
+//
+//  PopupManager.swift
+//  SwiftUICleanArchitect
+//
+//  Created by Truong Quoc Tai on 23/04/2024.
+//
+
+import SwiftUI
+
+open class PopupManager: ObservableObject {
+    @Published open var isShowingPopup = false
+    @Published open var popupContent: AnyView = .init(EmptyView())
+    
+    public init() {}
+
+    open func showPopup<Content: View>(_ view: Content) {
+        if isShowingPopup { return }
+        popupContent = AnyView(view)
+        withAnimation(.easeIn(duration: 0.3)) {
+            isShowingPopup = true
+        }
+    }
+
+    open func hidePopup() {
+        withAnimation(.easeOut(duration: 0.2)) {
+            isShowingPopup = false
+        }
+    }
+}
+
